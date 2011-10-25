6e3c20a02ba3313b85dd61883b60cf8f0c28679f
Daniel Parnell <me@danielparnell.com>
2011-10-25 15:19:05 +1100

    Removed the inline image display which was causing problems for me.

--- util/fold-dwim.el
+++ util/fold-dwim.el
@@ -145,7 +145,7 @@
 
 (eval-when-compile (require 'foldit))
 (eval-when-compile (require 'html-write))
-(eval-when-compile (require 'inlimg))
+;; (eval-when-compile (require 'inlimg))
 (eval-when-compile (require 'org))
 (eval-when-compile (require 'tex-mode))
 (require 'outline)
@@ -356,8 +356,8 @@ for this function from it."
   (cond
    ((get-char-property (point) 'html-write)
     (html-write-toggle-current-tag))
-   ((get-char-property (point) 'inlimg-img)
-    (inlimg-toggle-display (point)))
+;;   ((get-char-property (point) 'inlimg-img)
+;;    (inlimg-toggle-display (point)))
    ((eq major-mode 'org-mode)
     (org-cycle))
    ((and (fboundp 'outline-cycle)
